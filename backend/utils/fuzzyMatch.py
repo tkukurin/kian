2c525dec020c7163546dc6baf568ce510413ca75
Toni Kukurin <tkukurin@gmail.com>
2021-04-27 19:21:18 +0200

    different fuzzy match

--- backend/utils/fuzzyMatch.py
+++ backend/utils/fuzzyMatch.py
@@ -1,21 +1,23 @@
-# import hgtk
+
+def fuzzymatch_score(search_term_cased: str, with_ix=False):
+  search_term = search_term_cased.lower()
+  def fuzzy_inner(test_against: str, start=0):
+    test_against = test_against.lower()
+    score = 0
+    for iterm, char in enumerate(search_term):
+      if (last_match := test_against.find(char, start)) < 0: return 0
+      score += 1.0 / (last_match - start + 1)
+      start = last_match + 1
+    return (i, score) if with_ix else score
+  return fuzzy_inner if search_term else (lambda x: 1)
 
 
 def fuzzyMatch(needle, haystack):
-  #needle = hgtk.text.decompose(needle).lower()
-  #haystack = hgtk.text.decompose(haystack).lower()
+  score = fuzzymatch_score(needle)(haystack)
+  return score > 0
 
-  needle=needle.lower()
-  haystack=haystack.lower()
+if __name__ == '__main__':
+  # "Unit test" lol
+  print(fuzzyMatch('xyz', 'asdfxyyyybtytbz'), 'should be T')
+  print(fuzzyMatch('xyz', 'asdfxyyyybtytb'), 'should be F')
 
-  try:
-    idx = 0
-    for ch in needle:
-      while True:
-        if haystack[idx] == ch:
-          break
-        idx += 1
-      idx += 1
-    return True
-  except IndexError:
-    return False
