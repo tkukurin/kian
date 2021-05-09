931cf6cb9b17cd73304100a7c5d540a406b643fe
Toni Kukurin <tkukurin@gmail.com>
2021-05-09 17:35:18 +0200

    choose decks hack (no context)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,63 @@
-import {React, useState} from 'react';
 import './App.css';
 import MContext from './index.js';
+import React, {useState} from 'react';
+import {setMainSocket, listDeck, getReviewerNextEntry} from './call.ts';
+import katex from 'katex';
 
-function App(card) {
+/**
+ * Fixup Katex expressions which were rendered using some other page's CSS.
+ * Basically it seems Katex renders in 2 blocks:
+ * <span class='katex'>
+ *   <math> ... <annotation> {{latex}} </annotation> ...
+ *   <span class='katex-html'> ... </span>
+ * </span>
+ * We need to replace '.katex-html' inner HTML with re-rendered 'annotation'.
+ */
+function processHtml(htmlString) {
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(htmlString, 'text/html');
+  let katexs = doc.querySelectorAll('.katex');
+  for (let kx of katexs) {
+    let realTex = kx.querySelector('annotation').innerHTML;
+    let rendered = kx.querySelector('.katex-html');
+    // NOTE(tk) node is mutable, so this works.
+    rendered.innerHTML = katex.renderToString(realTex);
+  }
+  return doc.children[0].innerHTML;
+}
+
+function App({decksInit, cardInit}) {
+  const defaultCard = {front: 'Done', back: 'No more cards to review.'};
+  const [decks, setDecks] = useState(decksInit);
+  const [deck, setDeck] = useState(decks[0]);
   const [isActive, setActive] = useState(false);
+  const [card, setCard] = useState(cardInit || defaultCard);
+
+  function update(card) {
+    setCard(card || defaultCard);
+    setActive(false);
+  }
+
+  function clickDeck(e) {
+    console.log(e);
+    setDeck(e.target.innerText);
+    getReviewerNextEntry(deck).then(update);
+  }
+
   return (
     <div className="App">
-      <MContext.Consumer>
-        {context => (
-          <div>
-            <a dangerouslySetInnerHTML={{__html:context.state.front}} href="#"
-              className={isActive ? "hidden" : ""}
-              onClick={evt => setActive(true)} />
-            <div className={isActive ? "" : "hidden"}
-              dangerouslySetInnerHTML={{__html:context.state.back}} />
-          </div>
-        )}
-      </MContext.Consumer>
+      <ul>{decks.map(d => <li><a href='#' onClick={clickDeck}>{d}</a></li>)}</ul>
+      <p>Deck: {deck}</p>
+      <a href="#" onClick={e => getReviewerNextEntry(deck).then(update)}>
+        Reload
+      </a>
+      <div>
+        <a dangerouslySetInnerHTML={{__html:processHtml(card.front)}} href="#"
+          className={isActive ? "hidden" : ""}
+          onClick={evt => setActive(true)} />
+        <div className={isActive ? "" : "hidden"}
+          dangerouslySetInnerHTML={{__html:processHtml(card.back)}} />
+      </div>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,28 @@
+import './index.css';
+import 'katex/dist/katex.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import io from 'socket.io-client'
 import {setMainSocket, listDeck, getReviewerNextEntry} from './call.ts';
 
-// Woohoo this actually works
+// renders superscript as subscript ??
+// example to test Katex:
+// import katex from 'katex';
+// <div dangerouslySetInnerHTML={{__html:katex.renderToString('A=S^{-1}D_{-1}S')}} />
 
-
-const MContext = React.createContext();
 setMainSocket(io('http://localhost:28735/'))
-listDeck().then(d => console.log('eyo', d));
-getReviewerNextEntry('DeepMind').then(n => {
-  let state = {html:n};
-  console.log(state);
-  ReactDOM.render(
-    <React.StrictMode>
-      <MContext.Provider value={{state:n}}>
-        <a href="#" onClick={e => listDeck().then(d => console.log(d))}>Reload</a>
-        <App card={state} />
-      </MContext.Provider>
-    </React.StrictMode>,
-    document.getElementById('root'));
+listDeck().then(d => {
+  return getReviewerNextEntry(d[0]).then(n => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App decksInit={d} cardInit={n} />
+      </React.StrictMode>,
+      document.getElementById('root'));
+  })
 });
-
-export default MContext;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
